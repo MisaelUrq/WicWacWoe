a6a2ef4a944e7ea1def35be3c3a86ced7a421ada
Misael Urquieta <misaelurquieta198@hotmail.com>
2020-02-22 12:23:09 -0600

    We have switch to a batch file build

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
 @echo off
-c:\tools\compy.exe build
+if not exist obj mkdir obj
+if not exist bin mkdir bin
+
+SET COMMMON_FLAGS=-nologo -Zi -W4 -EHsc -Od -DDEBUG
+rem SET COMMMON_FLAGS=-nologo -W4 -EHsc -O2
+
+cl %COMMMON_FLAGS% src/win32_main.cpp -Febin/win32_WicWacWoe.exe -Ilib/ %OBJ_FILES% -Foobj/ User32.lib Gdi32.lib -link -SUBSYSTEM:WINDOWS -incremental:no -opt:ref

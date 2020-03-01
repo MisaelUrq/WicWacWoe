f84a3e1f161fdd5365e52160c309d032fba326db
Misael Urquieta <misaelurquieta198@hotmail.com>
2020-02-29 22:48:29 -0600

    DrawPoint

--- src/win32_main.cpp
+++ src/win32_main.cpp
@@ -11,14 +11,14 @@ LRESULT WINAPI Win32WindowsProc(HWND, UINT, WPARAM, LPARAM);
 struct PlatformInput {
     v2f mouse_position;
     u32 mouse_events;
-
+    
     bool is_down;
     bool is_up;
     bool is_left;
     bool is_right;
     bool spacebar;
     bool restart;
-
+    
     PlatformInput() {
         memset(this, 0, sizeof(PlatformInput));
     }
@@ -30,7 +30,7 @@ struct PlatformScreenBuffer
     u32   width;
     u32   height;
     u32   pitch;
-
+    
     PlatformScreenBuffer(const u32 width, const u32 height)
     {
         this->width  = width;
@@ -38,11 +38,11 @@ struct PlatformScreenBuffer
         this->pitch  = width*BYTES_PER_PIXEL;
         this->memory = operator new(width*height*BYTES_PER_PIXEL);
     }
-
+    
     ~PlatformScreenBuffer() {
         operator delete(this->memory);
     }
-
+    
     virtual void Resize(const u32 w, const u32 h) {
         this->width  = w;
         this->height = h;
@@ -55,9 +55,9 @@ struct PlatformScreenBuffer
 struct Win32ScreenBuffer : PlatformScreenBuffer
 {
     BITMAPINFO info;
-
+    
     Win32ScreenBuffer(const u32 width, const u32 height) :
-        PlatformScreenBuffer(width, height)
+    PlatformScreenBuffer(width, height)
     {
         info.bmiHeader.biSize = sizeof(info.bmiHeader);
         info.bmiHeader.biWidth  = width;
@@ -66,7 +66,7 @@ struct Win32ScreenBuffer : PlatformScreenBuffer
         info.bmiHeader.biBitCount = 32;
         info.bmiHeader.biCompression = BI_RGB;
     }
-
+    
     // TODO(Misael): Does this actually works?
     void Resize(const u32 w, const u32 h) override {
         PlatformScreenBuffer::Resize(w, h);
@@ -84,17 +84,17 @@ struct Line {
 struct Renderer {
     PlatformScreenBuffer* screen_buffer;
     std::vector<Line>     lines;
-
+    
     Renderer(PlatformScreenBuffer* screen_buffer)
     {
         this->screen_buffer = screen_buffer;;
     }
-
+    
     void StartFrame() {
         lines.clear();
         Clear(0xFF111111);
     }
-
+    
     // TODO(Misael): This really should not be name like this, this is
     // more of a render call. But maybe here it's when sorting or some
     // other stuff can happen
@@ -103,34 +103,39 @@ struct Renderer {
             DrawLine(*iter);
         }
     }
-
+    
     void PushSquare(v2f p, u32 d, u32 color) {
         PushLine(v2f(p.x - d, p.y - d), v2f(p.x - d, p.y + d), color);
         PushLine(v2f(p.x - d, p.y + d), v2f(p.x + d, p.y + d), color);
         PushLine(v2f(p.x + d, p.y + d), v2f(p.x + d, p.y - d), color);
         PushLine(v2f(p.x + d, p.y - d), v2f(p.x - d, p.y - d), color);
     }
-
+    
     void PushCross(v2f p, u32 d, u32 color) {
         PushLine(v2f(p.x - d, p.y - d), v2f(p.x + d, p.y + d), color);
         PushLine(v2f(p.x - d, p.y + d), v2f(p.x + d, p.y - d), color);
     }
-
+    
     void PushLine(v2f p1, v2f p2, u32 color) {
         lines.push_back({p1, p2, color});
     }
-
+    
+    void DrawPoint(f32 x, f32 y, u32 color) {
+        PlatformScreenBuffer* buffer = screen_buffer;
+        u32 *pixel = (u32*)buffer->memory;
+        if (x < buffer->width && x >= 0 && y >= 0 && y < buffer->height) {
+            pixel[(u32)y*buffer->width+(u32)x] = color;
+        }
+    }
+    
     // TODO(Misael): This method is not that great, lock for a beter
     // one.
     void DrawLine(const Line& line) {
-        PlatformScreenBuffer* buffer = screen_buffer;
-        Assert(buffer->memory);
-        u32 *pixel = (u32*)buffer->memory;
         const v2f *p1 = &line.p1;
         const v2f *p2 = &line.p2;
-
+        
         f32 m = (p1->x != p2->x) ? (p2 ->y - p1->y) / (p2->x - p1->x) : 0.0f;
-
+        
         // TODO(Misael): Add color to this.
         if (p1->x != p2->x && std::abs(m) <= 1.0f) {
             if (p1->x > p2->x) {
@@ -139,7 +144,7 @@ struct Renderer {
             const f32 b = p1->y - m * p1->x;
             FOR_RANGE((u32)p1->x, (u32)p2->x) {
                 f32 y = m * (f32)index + b;
-                pixel[(u32)y*buffer->width+index] = line.color;
+                DrawPoint((f32)index, y, line.color);
             }
         } else {
             if (p1->y > p2->y) {
@@ -149,12 +154,12 @@ struct Renderer {
             const f32 b = p1->x - m * p1->y;
             FOR_RANGE((u32)p1->y, (u32)p2->y) {
                 f32 x = m * (f32)index + b;
-                pixel[index*buffer->width+(u32)x] = line.color;
+                DrawPoint(x, (f32)index, line.color);
             }
         }
     }
-
-private:
+    
+    private:
     // TODO(Misael): Use SIMD instrucctions to accelerate this.
     void Clear(const u32 color) {
         u32* pixel = (u32*)screen_buffer->memory;
@@ -181,7 +186,7 @@ struct Game {
         this->current_piece    =  1;
         this->is_game_done     =  false;
     }
-
+    
     v2f GetPositionFromBoard(u32 position) {
         f32 w = (f32)renderer.screen_buffer->width;
         f32 h = (f32)renderer.screen_buffer->height;
@@ -199,19 +204,19 @@ struct Game {
         }
         return v2f(x, y);
     }
-
+    
     void AddSquare(u32 position) {
         renderer.PushSquare(GetPositionFromBoard(position), 50, 0xFF0000FF);
     }
-
+    
     void AddCross(u32 position) {
         renderer.PushCross(GetPositionFromBoard(position), 50, 0xFFFF0000);
     }
-
+    
     void AddPieceToBoard(u32 position, u32 type) {
         board_pieces[position] = type;
     }
-
+    
     void UpdateLogic() {
         if (input->restart) {
             memset(board_pieces, 0, sizeof(board_pieces));
@@ -220,7 +225,7 @@ struct Game {
             is_game_done   = false;
             input->restart = false;
         }
-
+        
         if (is_game_done == false) {
             if (input->is_left) {
                 if (current_position > 0) {
@@ -244,7 +249,7 @@ struct Game {
                 }
                 input->is_right = false;
             }
-
+            
             if (input->is_up) {
                 if (current_position > 2) {
                     if (board_pieces[current_position-3] == 0) {
@@ -265,7 +270,7 @@ struct Game {
                 }
                 input->is_down = false;
             }
-
+            
             if (input->spacebar) {
                 board_pieces[current_position] = current_piece;
                 current_piece = (current_piece == 1) ? 2 : 1;
@@ -291,7 +296,7 @@ struct Game {
             }
         }
     }
-
+    
     bool CheckForWinCases() {
         FOR_RANGE(0, 3) {
             u32 a = index * 3;
@@ -302,7 +307,7 @@ struct Game {
                 renderer.PushLine(GetPositionFromBoard(index*3), GetPositionFromBoard(index*3+2), 0xFF00FF00);
                 return true;
             }
-
+            
         }
         FOR_RANGE(0, 3) {
             u32 a = index;
@@ -330,7 +335,7 @@ struct Game {
         }
         return false;
     }
-
+    
     void UpdateFrame() {
         renderer.StartFrame();
         PushBoard();
@@ -355,7 +360,7 @@ struct Game {
         }
         renderer.EndFrame();
     }
-
+    
     void PushBoard() {
         FOR_RANGE(0, 2) {
             f32 y = (renderer.screen_buffer->height / 3.0f) * (f32)(index+1);
@@ -375,7 +380,7 @@ int WinMain(HINSTANCE instance, HINSTANCE ,
             LPSTR     , int      )
 {
     WNDCLASSEX wc;
-
+    
     wc.cbSize         = sizeof(WNDCLASSEX);
     wc.style          = CS_CLASSDC;
     wc.lpfnWndProc    = Win32WindowsProc;
@@ -388,7 +393,7 @@ int WinMain(HINSTANCE instance, HINSTANCE ,
     wc.lpszMenuName   = NULL;
     wc.lpszClassName  = "WicWacWoe_window_class";
     wc.hIconSm        = NULL;
-
+    
     if (RegisterClassEx(&wc) != 0)
     {
         u32 window_width  = 1280;
@@ -396,7 +401,7 @@ int WinMain(HINSTANCE instance, HINSTANCE ,
         HWND window_handle = CreateWindowEx(0, wc.lpszClassName, "WicWacWoe",
                                             WS_OVERLAPPEDWINDOW, 0, 0, window_width, window_height,
                                             NULL, NULL, instance, NULL);
-
+        
         if (window_handle)
         {
             global_offscreen_buffer = new Win32ScreenBuffer(window_width, window_height);
@@ -405,7 +410,7 @@ int WinMain(HINSTANCE instance, HINSTANCE ,
             global_is_app_running = true;
             ShowWindow(window_handle, SW_SHOWDEFAULT);
             UpdateWindow(window_handle);
-
+            
             while (global_is_app_running)
             {
                 {
@@ -414,95 +419,95 @@ int WinMain(HINSTANCE instance, HINSTANCE ,
                     {
                         switch (msg.message)
                         {
-                        case WM_QUIT:
-                        {
-                            global_is_app_running = false;
-                        } break;
-                        case WM_SYSKEYDOWN:
-                        case WM_SYSKEYUP:
-                        case WM_KEYDOWN:
-                        case WM_KEYUP:
-                        {
-                            u32 vkcode = (u32)msg.wParam;
-                            b32 was_down = ((msg.lParam & (1 << 30)) != 0);
-                            b32 is_down  = ((msg.lParam & (1 << 31)) == 0);
-                            bool alt_key_was_down = (msg.lParam & (1 << 29));
-
-                            if (!alt_key_was_down && vkcode == VK_CONTROL) {
-                                MSG next_msg;
-                                if (PeekMessage(&next_msg, NULL, 0, 0, PM_NOREMOVE) && next_msg.message == msg.message) {
-                                    // Nothing?
-                                    global_ctr_key_was_down = false;
-                                } else if (global_ctr_key_was_down && !(is_down)) {
-                                    global_ctr_key_was_down = false;
-                                }
-                                else {
-                                    global_ctr_key_was_down = true;
-                                }
-                            }
-
-                            if(was_down != is_down)
+                            case WM_QUIT:
                             {
-                                if (is_down)
+                                global_is_app_running = false;
+                            } break;
+                            case WM_SYSKEYDOWN:
+                            case WM_SYSKEYUP:
+                            case WM_KEYDOWN:
+                            case WM_KEYUP:
+                            {
+                                u32 vkcode = (u32)msg.wParam;
+                                b32 was_down = ((msg.lParam & (1 << 30)) != 0);
+                                b32 is_down  = ((msg.lParam & (1 << 31)) == 0);
+                                bool alt_key_was_down = (msg.lParam & (1 << 29));
+                                
+                                if (!alt_key_was_down && vkcode == VK_CONTROL) {
+                                    MSG next_msg;
+                                    if (PeekMessage(&next_msg, NULL, 0, 0, PM_NOREMOVE) && next_msg.message == msg.message) {
+                                        // Nothing?
+                                        global_ctr_key_was_down = false;
+                                    } else if (global_ctr_key_was_down && !(is_down)) {
+                                        global_ctr_key_was_down = false;
+                                    }
+                                    else {
+                                        global_ctr_key_was_down = true;
+                                    }
+                                }
+                                
+                                if(was_down != is_down)
                                 {
-                                    if (global_ctr_key_was_down)
+                                    if (is_down)
                                     {
-                                        // TODO(Misael): Code with ctr presses....
-                                    }
-                                    else if (alt_key_was_down)
-                                    {
-                                        // TODO(Misael): Alt was down....
-                                    }
-                                    else
-                                    {
-                                        // TODO(Misael): Normal keys....
-                                        switch (vkcode)
+                                        if (global_ctr_key_was_down)
                                         {
-                                        case VK_UP:
-                                        {
-                                            input->is_up = is_down;
+                                            // TODO(Misael): Code with ctr presses....
                                         }
-                                        break;
-                                        case VK_DOWN:
+                                        else if (alt_key_was_down)
                                         {
-                                            input->is_down = is_down;
+                                            // TODO(Misael): Alt was down....
                                         }
-                                        break;
-                                        case VK_RIGHT:
+                                        else
                                         {
-                                            input->is_right = is_down;
-                                        }
-                                        break;
-                                        case VK_LEFT:
-                                        {
-                                            input->is_left = is_down;
-                                        }
-                                        break;
-                                        case VK_SPACE:
-                                        {
-                                            input->spacebar = is_down;
-                                        }
-                                        break;
-                                        case VK_ESCAPE:
-                                        {
-                                            input->restart = is_down;
-                                        }
-                                        break;
+                                            // TODO(Misael): Normal keys....
+                                            switch (vkcode)
+                                            {
+                                                case VK_UP:
+                                                {
+                                                    input->is_up = is_down;
+                                                }
+                                                break;
+                                                case VK_DOWN:
+                                                {
+                                                    input->is_down = is_down;
+                                                }
+                                                break;
+                                                case VK_RIGHT:
+                                                {
+                                                    input->is_right = is_down;
+                                                }
+                                                break;
+                                                case VK_LEFT:
+                                                {
+                                                    input->is_left = is_down;
+                                                }
+                                                break;
+                                                case VK_SPACE:
+                                                {
+                                                    input->spacebar = is_down;
+                                                }
+                                                break;
+                                                case VK_ESCAPE:
+                                                {
+                                                    input->restart = is_down;
+                                                }
+                                                break;
+                                            }
                                         }
                                     }
                                 }
                             }
-                        }
-                        break;
+                            break;
                         }
                         TranslateMessage(&msg);
                         DispatchMessage(&msg);
                     }
                 }
-
+                
                 game.UpdateLogic();
                 game.UpdateFrame();
-
+                
                 HDC dc = GetDC(window_handle);
                 StretchDIBits(dc,
                               0, 0, 1280, 780,
@@ -510,7 +515,7 @@ int WinMain(HINSTANCE instance, HINSTANCE ,
                               global_offscreen_buffer->memory, &global_offscreen_buffer->info, DIB_RGB_COLORS, SRCCOPY);
                 ReleaseDC(window_handle, dc);
             }
-
+            
             delete input;
             delete global_offscreen_buffer;
             DestroyWindow(window_handle);
@@ -533,21 +538,21 @@ LRESULT WINAPI Win32WindowsProc(HWND handle, UINT msg, WPARAM w_param, LPARAM l_
 {
     switch (msg)
     {
-    case WM_PAINT:
-    {
-        PAINTSTRUCT paint;
-        HDC dc = BeginPaint(handle, &paint);
-        StretchDIBits(dc,
-                      0, 0, 1280, 780,
-                      0, 0, global_offscreen_buffer->width, global_offscreen_buffer->height,
-                      global_offscreen_buffer->memory, &global_offscreen_buffer->info, DIB_RGB_COLORS, SRCCOPY);
-        EndPaint(handle, &paint);
-    }
-    break;
-    case WM_CLOSE:
+        case WM_PAINT:
+        {
+            PAINTSTRUCT paint;
+            HDC dc = BeginPaint(handle, &paint);
+            StretchDIBits(dc,
+                          0, 0, 1280, 780,
+                          0, 0, global_offscreen_buffer->width, global_offscreen_buffer->height,
+                          global_offscreen_buffer->memory, &global_offscreen_buffer->info, DIB_RGB_COLORS, SRCCOPY);
+            EndPaint(handle, &paint);
+        }
+        break;
+        case WM_CLOSE:
         global_is_app_running = false;
         break;
-    case WM_DESTROY:
+        case WM_DESTROY:
         PostQuitMessage(0);
         break;
     }
